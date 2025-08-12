54e3cf2a09e754a29294b437c82165c7df14483f
Marco Fontana <marco.fontana98@outlook.it>
2025-08-12 15:00:28 +0200

    feat(api_gateway): add controller interface

---
+++ api_gateway/src/main/java/cce/api_gateway/application/Controller.java
@@ -0,0 +1,12 @@
+package cce.api_gateway.application;
+
+import io.vertx.circuitbreaker.CircuitBreaker;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.WebClient;
+
+public interface Controller {
+    void rerouteTo(Router router, String endpoint, String host, WebClient client);
+    Handler<RoutingContext> healthCheckHandler(WebClient client, String host);
+}
