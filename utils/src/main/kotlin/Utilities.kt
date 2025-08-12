9f0bb675e852a23fefa2b9c6e4745852d3f26cf3
Marco Fontana <marco.fontana98@outlook.it>
2025-08-12 14:57:53 +0200

    feat(utils): add HTTP Status codes

--- utils/src/main/kotlin/Utilities.kt
+++ utils/src/main/kotlin/Utilities.kt
@@ -1,17 +1,15 @@
 package mf.cce.utils
 
-import kotlinx.datetime.*
-import kotlinx.serialization.Serializable
-import kotlinx.coroutines.*
-
-@Serializable
-class Printer(val message: String) {
-    fun printMessage() = runBlocking {
-        val now: Instant = Clock.System.now()
-        launch {
-            delay(1000L)
-            println(now.toString())
-        }
-        println(message)
-    }
+object HttpStatus {
+    const val OK = 200
+    const val CREATED = 201
+    const val ACCEPTED = 202
+    const val NO_CONTENT = 204
+    const val BAD_REQUEST = 400
+    const val UNAUTHORIZED = 401
+    const val FORBIDDEN = 403
+    const val NOT_FOUND = 404
+    const val CONFLICT = 409
+    const val INTERNAL_SERVER_ERROR = 500
+    const val SERVICE_UNAVAILABLE = 503
 }
