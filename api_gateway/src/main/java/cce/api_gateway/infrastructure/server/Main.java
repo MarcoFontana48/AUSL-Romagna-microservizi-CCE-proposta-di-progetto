a418cdf2dbe93bf2040d43f71ad29e801334b2ac
Marco Fontana <marco.fontana98@outlook.it>
2025-08-12 15:01:38 +0200

    feat(api_gateway): add new main class

---
+++ api_gateway/src/main/java/cce/api_gateway/infrastructure/server/Main.java
@@ -0,0 +1,22 @@
+package cce.api_gateway.infrastructure.server;
+
+import io.vertx.core.Vertx;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    
+    public static void main(String[] args) {
+        LOGGER.info("Starting server...");
+        runServer();
+    }
+    
+    private static void runServer() {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new ApiGatewayVerticle()).onSuccess(id -> LOGGER.info("Server started successfully with deployment ID: {}", id)).onFailure(throwable -> {
+            LOGGER.error("Failed to start server: {}", throwable.getMessage());
+            vertx.close();
+        });
+    }
+}
