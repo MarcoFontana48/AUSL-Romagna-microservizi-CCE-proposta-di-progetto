254f71b77e9ca75b4cf386ad7b37f9470355a3cb
Marco Fontana <marco.fontana98@outlook.it>
2025-08-12 17:38:18 +0200

    chore(build): update service module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
-include(":app")
+include(":service")
 include(":utils")
 
 rootProject.name = "CCE_Microservizi"
