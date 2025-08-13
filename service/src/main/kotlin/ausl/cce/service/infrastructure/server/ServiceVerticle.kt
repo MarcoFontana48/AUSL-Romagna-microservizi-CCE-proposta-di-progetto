67f6ca101c4efe1a0e0bb6df7bd7e89148859a98
Marco Fontana <marco.fontana98@outlook.it>
2025-08-13 11:13:15 +0200

    refactor(service): make start server clearer to understand

--- service/src/main/kotlin/ausl/cce/service/infrastructure/server/ServiceVerticle.kt
+++ service/src/main/kotlin/ausl/cce/service/infrastructure/server/ServiceVerticle.kt
@@ -5,6 +5,8 @@ import ausl.cce.service.infrastructure.controller.StandardController
 import io.vertx.circuitbreaker.CircuitBreaker
 import io.vertx.circuitbreaker.CircuitBreakerOptions
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.http.HttpServer
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
 import mf.cce.utils.Endpoints
@@ -18,29 +20,37 @@ class ServiceVerticle : AbstractVerticle() {
     override fun start() {
         logger.info("Starting service...")
 
+        val circuitBreaker = defineCircuiteBreaker()
+        val controller: ServiceController = StandardController(circuitBreaker)
+        val router = Router.router(vertx)
+        defineEndpoints(router, controller)
+        runServer(router).onSuccess {
+            logger.info("Service started successfully, listening on port ${Ports.HTTP}")
+        }
+        .onFailure { throwable ->
+            logger.error("Failed to start service: ${throwable.message}")
+        }
+    }
+
+    private fun defineCircuiteBreaker(): CircuitBreaker {
         val options = CircuitBreakerOptions()
             .setMaxFailures(5)
             .setTimeout(10000)
             .setResetTimeout(30000)
 
-        val circuitBreaker = CircuitBreaker.create("service-circuit-breaker", this.vertx, options)
+        return CircuitBreaker.create("service-circuit-breaker", this.vertx, options)
+    }
 
-        val controller: ServiceController = StandardController(circuitBreaker)
+    private fun runServer(router: Router): Future<HttpServer> {
+        return this.vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(Ports.HTTP)
+    }
 
-        val router = Router.router(vertx)
+    private fun defineEndpoints(router: Router, controller: ServiceController) {
         router.route().handler(BodyHandler.create())
 
         router.get(Endpoints.HEALTH).handler(controller.healthCheckHandler())
         router.get(Endpoints.METRICS).handler(controller.metricsHandler())
-
-        this.vertx.createHttpServer()
-            .requestHandler(router)
-            .listen(Ports.HTTP)
-            .onSuccess {
-                logger.info("Service started successfully, listening on port ${Ports.HTTP}")
-            }
-            .onFailure { throwable ->
-                logger.error("Failed to start service: ${throwable.message}")
-            }
     }
 }
