b860c3021dcbe32f17549949a8124074d522152c
Marco Fontana <marco.fontana98@outlook.it>
2025-08-13 11:01:53 +0200

    feat(utils): add metrics registry

--- utils/src/main/kotlin/Metrics.kt
+++ utils/src/main/kotlin/Metrics.kt
@@ -3,38 +3,67 @@ package mf.cce.utils
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Timer
-import kotlin.getValue
 
-abstract class AbstractRegistryController(
-    private val serviceName: String
-) {
-    protected abstract val meterRegistry: MeterRegistry
+/**
+ * Health check metrics interface with default implementation
+ */
+interface HealthCheckMetricsProvider {
+    val meterRegistry: MeterRegistry
+    val serviceName: String
 
-    protected val healthCheckCounter by lazy {
-        Counter.builder("health_check_requests_total")
+    val healthCheckCounter: Counter
+        get() = Counter.builder("health_check_requests_total")
             .description("Total number of health check requests")
             .tag("service", serviceName)
             .register(meterRegistry)
-    }
 
-    protected val healthCheckSuccessCounter by lazy {
-        Counter.builder("health_check_success_total")
+    val healthCheckSuccessCounter: Counter
+        get() = Counter.builder("health_check_success_total")
             .description("Total number of successful health checks")
             .tag("service", serviceName)
             .register(meterRegistry)
-    }
 
-    protected val healthCheckFailureCounter by lazy {
-        Counter.builder("health_check_failure_total")
+    val healthCheckFailureCounter: Counter
+        get() = Counter.builder("health_check_failure_total")
             .description("Total number of failed health checks")
             .tag("service", serviceName)
             .register(meterRegistry)
-    }
 
-    protected val healthCheckTimer by lazy {
-        Timer.builder("health_check_duration_seconds")
+    val healthCheckTimer: Timer
+        get() = Timer.builder("health_check_duration_seconds")
             .description("Health check request duration")
             .tag("service", serviceName)
             .register(meterRegistry)
-    }
+}
+
+/**
+ * General metrics interface with default implementation
+ */
+interface MetricsProvider {
+    val meterRegistry: MeterRegistry
+    val serviceName: String
+
+    val metricsCounter: Counter
+        get() = Counter.builder("metrics_requests_total")
+            .description("Total number of metrics requests")
+            .tag("service", serviceName)
+            .register(meterRegistry)
+
+    val metricsSuccessCounter: Counter
+        get() = Counter.builder("metrics_success_total")
+            .description("Total number of successful metrics")
+            .tag("service", serviceName)
+            .register(meterRegistry)
+
+    val metricsFailureCounter: Counter
+        get() = Counter.builder("metrics_failure_total")
+            .description("Total number of failed metrics")
+            .tag("service", serviceName)
+            .register(meterRegistry)
+
+    val metricsTimer: Timer
+        get() = Timer.builder("metrics_duration_seconds")
+            .description("Metrics request duration")
+            .tag("service", serviceName)
+            .register(meterRegistry)
 }
