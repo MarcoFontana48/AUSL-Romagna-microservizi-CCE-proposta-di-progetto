eef955cf2263063d506c584328f526f9e1e672ab
Marco Fontana <marco.fontana98@outlook.it>
2025-08-15 17:39:11 +0200

    feat(utils): add log4j2 library dependency

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
     implementation(libs.micrometer.core)
     implementation(libs.micrometer.registry.prometheus)
     implementation(libs.micrometer.registry.jmx)
+    implementation(libs.log4j.api)
+    implementation(libs.log4j.core)
     testImplementation(kotlin("test"))
 }
 
