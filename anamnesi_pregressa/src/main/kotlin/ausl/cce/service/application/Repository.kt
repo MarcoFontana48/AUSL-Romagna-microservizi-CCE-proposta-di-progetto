50fc8ce7c8513ce4a073000400ae7fb9d75ab66c
Marco Fontana <marco.fontana98@outlook.it>
2025-09-02 15:44:29 +0200

    feat(anamnesi_pregressa): add mongo repository for allergies

--- anamnesi_pregressa/src/main/kotlin/ausl/cce/service/application/Repository.kt
+++ anamnesi_pregressa/src/main/kotlin/ausl/cce/service/application/Repository.kt
@@ -1,10 +1,14 @@
-package ausl.cce.service.application
+package ausl.cce.service.infrastructure.persistence
 
+import ausl.cce.service.domain.AllergyIntoleranceEntity
+import ausl.cce.service.domain.AllergyIntoleranceId
 import ausl.cce.service.domain.DummyEntity
-import ausl.cce.service.domain.DummyEntity.DummyId
 import mf.cce.utils.Repository
 
-interface DummyRepository : Repository<DummyId, DummyEntity>, Closeable
+// Updated repository interfaces to include Closeable
+interface DummyRepository : Repository<DummyEntity.DummyId, DummyEntity>, Closeable
+
+interface AllergyIntoleranceRepository : Repository<AllergyIntoleranceId, AllergyIntoleranceEntity>, Closeable
 
 interface Closeable {
     fun close()
