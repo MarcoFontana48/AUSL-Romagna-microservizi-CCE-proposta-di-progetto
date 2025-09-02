a8c8e762bedfb972240dddbf2681501556c516a7
Marco Fontana <marco.fontana98@outlook.it>
2025-09-02 15:45:04 +0200

    feat(anamnesi_pregressa): add allergy ddd service object

--- anamnesi_pregressa/src/main/kotlin/ausl/cce/service/application/Service.kt
+++ anamnesi_pregressa/src/main/kotlin/ausl/cce/service/application/Service.kt
@@ -1,8 +1,39 @@
 package ausl.cce.service.application
 
+import ausl.cce.service.domain.AllergyIntoleranceEntity
+import ausl.cce.service.domain.AllergyIntoleranceId
 import ausl.cce.service.domain.DummyEntity
 import ausl.cce.service.domain.DummyEntity.DummyId
+import ausl.cce.service.infrastructure.persistence.AllergyIntoleranceRepository
+import ausl.cce.service.infrastructure.persistence.DummyRepository
 import mf.cce.utils.Service
+
+interface AllergyIntoleranceService : Service {
+    fun getAllergyIntoleranceById(id: AllergyIntoleranceId): AllergyIntoleranceEntity
+    fun addAllergyIntolerance(entity: AllergyIntoleranceEntity)
+    fun updateAllergyIntolerance(entity: AllergyIntoleranceEntity)
+    fun deleteAllergyIntolerance(id: AllergyIntoleranceId)
+}
+
+class AllergyIntoleranceServiceImpl(
+    private val allergyIntoleranceRepository: AllergyIntoleranceRepository,
+) : AllergyIntoleranceService {
+    override fun getAllergyIntoleranceById(id: AllergyIntoleranceId): AllergyIntoleranceEntity {
+        return allergyIntoleranceRepository.findById(id) ?: throw NoSuchElementException("AllergyIntoleranceEntity with id '$id' not found")
+    }
+
+    override fun addAllergyIntolerance(entity: AllergyIntoleranceEntity) {
+        allergyIntoleranceRepository.save(entity)
+    }
+
+    override fun updateAllergyIntolerance(entity: AllergyIntoleranceEntity) {
+        allergyIntoleranceRepository.update(entity)
+    }
+
+    override fun deleteAllergyIntolerance(id: AllergyIntoleranceId) {
+        allergyIntoleranceRepository.deleteById(id)
+    }
+}
 
 interface DummyService : Service {
     fun getDummyEntityById(id: DummyId): DummyEntity
@@ -12,7 +43,7 @@ interface DummyService : Service {
 }
 
 class DummyServiceImpl(
-    private val dummyRepository: DummyRepository
+    private val dummyRepository: DummyRepository,
 ) : DummyService {
     override fun getDummyEntityById(id: DummyId): DummyEntity {
         return dummyRepository.findById(id) ?: throw NoSuchElementException("DummyEntity with id '$id' not found")
