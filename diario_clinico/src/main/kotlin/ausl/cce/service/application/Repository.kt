25818956c1d655f79cd202c0607d67d3db1b44fc
Marco Fontana <marco.fontana98@outlook.it>
2025-09-04 12:40:43 +0200

    feat(diario_clinico): update repository interfaces to include Encounter repository and Closeable

--- diario_clinico/src/main/kotlin/ausl/cce/service/application/Repository.kt
+++ diario_clinico/src/main/kotlin/ausl/cce/service/application/Repository.kt
@@ -1,10 +1,14 @@
-package ausl.cce.service.application
+package ausl.cce.service.infrastructure.persistence
 
+import ausl.cce.service.domain.EncounterEntity
+import ausl.cce.service.domain.EncounterId
 import ausl.cce.service.domain.DummyEntity
-import ausl.cce.service.domain.DummyEntity.DummyId
 import mf.cce.utils.Repository
 
-interface DummyRepository : Repository<DummyId, DummyEntity>, Closeable
+// Updated repository interfaces to include Closeable
+interface DummyRepository : Repository<DummyEntity.DummyId, DummyEntity>, Closeable
+
+interface EncounterRepository : Repository<EncounterId, EncounterEntity>, Closeable
 
 interface Closeable {
     fun close()
