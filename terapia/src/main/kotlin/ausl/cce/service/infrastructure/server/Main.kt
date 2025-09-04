8f05b7a82b38a6ad18c805e209f6401e72b30b08
Marco Fontana <marco.fontana98@outlook.it>
2025-09-04 22:31:39 +0200

    feat(terapia): integrate CarePlanService into Main and update ServerVerticle initialization

--- terapia/src/main/kotlin/ausl/cce/service/infrastructure/server/Main.kt
+++ terapia/src/main/kotlin/ausl/cce/service/infrastructure/server/Main.kt
@@ -1,9 +1,13 @@
 package ausl.cce.service.infrastructure.server
 
-import ausl.cce.service.application.DummyRepository
+import ausl.cce.service.application.CarePlanService
+import ausl.cce.service.application.CarePlanServiceImpl
 import ausl.cce.service.application.DummyService
 import ausl.cce.service.application.DummyServiceImpl
-import ausl.cce.service.infrastructure.persistence.MongoRepository
+import ausl.cce.service.infrastructure.persistence.CarePlanRepository
+import ausl.cce.service.infrastructure.persistence.DummyRepository
+import ausl.cce.service.infrastructure.persistence.MongoCarePlanRepository
+import ausl.cce.service.infrastructure.persistence.MongoDummyRepository
 import io.vertx.core.Vertx
 import mf.cce.utils.RepositoryCredentials
 
@@ -23,10 +27,13 @@ fun runServer() {
         System.getenv("CONFIG_SERVER_DB_PASSWORD") ?: "password"
     )
 
-    val serviceRepository : DummyRepository = MongoRepository(mongoRepositoryCredentials)
-    val service : DummyService = DummyServiceImpl(serviceRepository)
+    val dummyServiceRepository : DummyRepository = MongoDummyRepository(mongoRepositoryCredentials)
+    val dummyService : DummyService = DummyServiceImpl(dummyServiceRepository)
 
-    val serverVerticle = ServerVerticle(service)
+    val carePlanServiceRepository: CarePlanRepository = MongoCarePlanRepository(mongoRepositoryCredentials)
+    val carePlanService : CarePlanService = CarePlanServiceImpl(carePlanServiceRepository)
+
+    val serverVerticle = ServerVerticle(dummyService, carePlanService)
 
     vertx.deployVerticle(serverVerticle).onSuccess {
         println("Service Verticle deployed successfully!")
